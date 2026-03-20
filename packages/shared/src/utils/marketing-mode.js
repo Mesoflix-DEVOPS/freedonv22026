ddfec73e9b1f5f5987ea55d86a9697c9295f6a4b
lemic murimi <lemicmurimivennry@gmail.com>
2026-03-21 02:13:55 +0300

    feat: update marketing mode balance range to ,300 - ,000

--- packages/shared/src/utils/marketing-mode.js
+++ packages/shared/src/utils/marketing-mode.js
@@ -26,8 +26,8 @@ export const getMaskedBalance = () => {
 
 export const resetMaskedBalance = () => {
     if (typeof window === 'undefined') return 0;
-    // Randomized balance between 1000 and 8000
-    const newBalance = Math.floor(Math.random() * (8000 - 1000 + 1)) + 1000;
+    // Randomized balance between 10300 and 18000
+    const newBalance = Math.floor(Math.random() * (18000 - 10300 + 1)) + 10300;
     localStorage.setItem(MASKED_BALANCE_KEY, newBalance.toString());
     return newBalance;
 };
